2893b598b3f7320ac6b9b3ad6e6f71e353573ef8
prawesh lama <prawesh lama>
2016-10-12 17:57:37 +0530

    created variables with different data types

--- js/script.js
+++ js/script.js
@@ -1,2 +1,40 @@
 // this is my java script. i want to see if its working.
-console.log("my javascript script is working.");
+//console.log("my javascript script is working.");
+
+// in java script there are five basic data types
+// 1) a string, which is a sequence of characters
+var myString = "hello world.";
+// console.log(myString);
+
+// 2) A number
+var myNumber = 24;
+// console.log(myNumber);
+
+// console.log(myNumber +70);
+// console.log(myString +70);
+// console.log(myNumber.toString() +70);
+
+//3 ) an array, which is a fancy word for a list
+
+var myArray =["Prawesh","Aloke","Anand"];
+// console.log(myArray);
+// console.log(myArray[1]);
+// console.log(myArray.length);
+myArray.push("Harry");
+
+// console.log(myArray);
+
+myArray.push(myNumber);
+// console.log(myArray);
+
+
+// 4) An object, which is a set of key-value pairs
+var myObject = {
+   name: " Harry",
+age : 29,
+classmates: myArray
+
+}
+
+// 5) A boolean, which is a true or false
+var myBool = true;
